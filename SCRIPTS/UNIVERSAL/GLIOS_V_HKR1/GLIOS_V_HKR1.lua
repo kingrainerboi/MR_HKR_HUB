a4ba2eebbf7a2f916e2400fba14e0ab664ac4fab
kingrainerboi <kingrainmendoza282000@gmail.com>
2025-05-19 03:36:20 -0400

    v2

--- SCRIPTS/UNIVERSAL/GLIOS_V_HKR1/GLIOS_V_HKR1.lua
+++ SCRIPTS/UNIVERSAL/GLIOS_V_HKR1/GLIOS_V_HKR1.lua
@@ -1,4 +1,3 @@
--- MR_HK_WORK.lua
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kingrainerboi/MR_HKR_HUB/refs/heads/master/SCRIPTS/UNIVERSAL/GLIOS_V_HKR1/MR_HK_WORK.lua"))()
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kingrainerboi/MR_HKR_HUB/refs/heads/master/SCRIPTS/UNIVERSAL/GLIOS_V_HKR1/MR_KR_WORK.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kingrainerboi/MR_HKR_HUB/refs/heads/master/SCRIPTS/UNIVERSAL/GLIOS_V_HKR1/MR_HK_WORK.lua"))()
