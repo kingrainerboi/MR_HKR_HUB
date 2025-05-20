fb3ee1f8be18a48cfb8479d0364f7641a905a02e
kingrainerboi <kingrainmendoza282000@gmail.com>
2025-05-20 02:48:26 -0400

    v1

---
+++ SCRIPTS/GAMES/JJK/rushed.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kingrainerboi/MR_HKR_HUB/refs/heads/master/SCRIPTS/GAMES/JJK/GLIOS.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kingrainerboi/MR_HKR_HUB/refs/heads/master/SCRIPTS/GAMES/JJK/glios_A.lua"))()
